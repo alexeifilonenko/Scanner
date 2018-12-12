7ac302c54f13c43fe68e757834dc4b9a2e5fe9eb
alexeifilonenko <alexeifilonenko@gmail.com>
2018-12-12 18:08:14 +0300

    the first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Read from file or Read from console?");
+        String s = scanner.nextLine();
+        if ("Read from file".equals(s)) {
+            System.out.println("Enter the file name with extension: ");
+            String str = scanner.nextLine();
+            String path = "C:/resources/file.txt";              //создаем переменную, в которую помещаем путь до нашего файла
+            File file = new File(path);                         //добавляем созданный файл в код
+            Scanner scanner1 = new Scanner(file);               //объекту класса Scanner передаем наш файл
+            //List<String> rows = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+            String line = scanner1.nextLine();
+            String[] words =line.split(" ");
+            System.out.println(Arrays.toString(words));
+
+        }else if ("Read from console".equals(s)) {
+            System.out.println("Type any symbol using your keyboard");
+            String str = scanner.nextLine();
+        }else {
+            System.out.println("Type only Read from file or Read from console");
+        }
+    }
+}
---
+++ src/com/company/test.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) throws Exception{
+        List<String> list = Files.readAllLines(Paths.get("file.txt"));
+        for(String item: list) {
+            System.out.println(item);
+        }
+    }
+}
